0dc5521cf5e0cca9e8e792f9e38b4403922c592f
Jayribyo <caribyo@gmail.com>
2026-03-02 11:37:58 +0100

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   output: "static",
-  site: "https://starcitizenbeginner.com", // <- deine echte Domain
+  site: "https://starcitizenbeginner.com",
+  integrations: [sitemap()],
 });
